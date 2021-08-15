111e03934914d07f6b6c0cac47ad4f9f56d88157
syumai <syumai@gmail.com>
2021-08-15 18:19:41 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# gospec-previewer
+
+* A simple web application to preview specified version of Go Language Specification.
+
+## Development
+
+```
+npx vercel dev
+```
+
+## License
+
+MIT
+
+## Author
+
+syumai
---
+++ api/spec/index.js
@@ -0,0 +1,84 @@
+//@ts-check
+
+const https = require("https");
+
+const refRegExp = /^[a-zA-Z0-9/.\-_]+$/;
+
+/**
+ * validateRef checks format of ref.
+ *
+ * @param {string} ref
+ * @returns boolean
+ */
+const validateRef = (ref) => {
+  return refRegExp.test(ref);
+};
+
+/**
+ * loadContent loads Go Spec HTML content and return it as plain HTML.
+ *
+ * @param {string} ref
+ * @returns {Promise<string>}
+ */
+const fetchContent = async (ref) => {
+  if (!validateRef(ref)) {
+    throw new Error(`invalid ref format: ${ref}`);
+  }
+  const url = `https://go.googlesource.com/go/+/${ref}/doc/go_spec.html?format=TEXT`;
+  const req = https.get(url);
+  /** @type {import("http").IncomingMessage} */
+  const res = await new Promise((resolve, reject) => {
+    req.on("response", (res) => resolve(res));
+    req.on("error", (err) => reject(err));
+  });
+  res.setEncoding("utf8");
+  const chunks = [];
+  for await (const chunk of res) {
+    chunks.push(chunk);
+  }
+  res.resume();
+  return Buffer.from(chunks.join(""), "base64").toString("utf8");
+};
+
+/**
+ * renderContent renders content as full HTML.
+ *
+ * @param {string} content
+ * @returns string
+ */
+const renderContent = (content) => {
+  const contentDesc = JSON.parse(content.match(/<!--([^>]*)-->/)[1]);
+  return `<!doctype html>
+<html>
+  <head>
+    <title>Go Spec Viewer</title>
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:600|Roboto:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Product+Sans&text=Supported%20by%20Google&display=swap" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="https://golang.org/lib/godoc/style.css">
+  </head>
+  <body>
+  <main id="page" class="Site-content wide">
+<div class="container">
+  <h1>
+    ${contentDesc.Title}
+    <span class="text-muted"></span>
+  </h1>
+  <h2>
+    ${contentDesc.Subtitle}
+    <span class="text-muted"></span>
+  </h2>
+  <div id="nav"></div>
+${content}
+  </main>
+  <script src="/src/toc.js"></script>
+  </body>
+</html>`;
+};
+
+module.exports = async (req, res) => {
+  const {
+    query: { ref },
+  } = req;
+  const content = await fetchContent(ref);
+  res.send(renderContent(content));
+};
